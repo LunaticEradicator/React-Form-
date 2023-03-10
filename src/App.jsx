76538ee806c9d5651b58aada1815f5185680d6c0
YeduKrishnan A <kuttuzz17@gmail.com>
2023-03-10 15:11:03 +0530

    React Form Submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [formData, setFormData] = React.useState(
+    {
+      emailId: "",
+      password: "",
+      confirmPassword: "",
+      isNewsletter: false
+    }
+  )
+
+  console.log(formData)
+
+  function handleClick(event) {
+    const { name, value, type, checked } = event.target;
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value
+      }
+    })
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // if (formData.password === formData.confirmPassword) {
+    //   console.log('Successfully Signed In')
+    // }
+    // else {
+    //   console.log('Passwords do not match')
+    //   return;
+    // }
+
+    formData.password === formData.confirmPassword ? console.log('Successfully Signed In') : console.log('Passwords do not match');
+
+    if (formData.isNewsletter === true && formData.password === formData.confirmPassword) {
+      console.log('Thanks for signing up for our newsletter');
+    }
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <form className="form" onSubmit={handleSubmit}>
+
+      <div className="emailId-div">
+        <label htmlFor="emailId"></label>
+        <input
+          type="text"
+          name="emailId"
+          id="emailId"
+          placeholder="Email ID"
+          onChange={handleClick}
+          value={formData.emailId} // for Controlling State
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="password-div">
+        <label htmlFor="password"></label>
+        <input type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          onChange={handleClick}
+          value={formData.password}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+
+      <div className="confirmPassword-div">
+        <label htmlFor="emailId"></label>
+        <input type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          placeholder="Confirm Password"
+          onChange={handleClick}
+          value={formData.confirmPassword}
+        />
+      </div>
+
+      <div className="newsletter-div">
+        <input type="checkbox"
+          name="isNewsletter"
+          id="isNewsletter"
+          onChange={handleClick}
+          checked={formData.isNewsletter}
+        />
+        <label htmlFor="isNewsletter">I want to join the newsletter</label>
+      </div>
+
+      <div className="signUp">
+        <button>Sign Up</button>
+      </div>
+
+    </form>
   )
 }
-
-export default App
---
+++ src/form.jsx
@@ -0,0 +1,54 @@
+// import React from "react";
+
+// export default Form(){
+//     return (
+//         <>
+//             <div className="emailId-div">
+//                 <label htmlFor="emailId"></label>
+//                 <input
+//                     type="text"
+//                     name="emailId"
+//                     id="emailId"
+//                     placeholder="Email ID"
+//                     onChange={handleClick}
+//                     value={formData.emailId} // for Controlling State
+//                 />
+//             </div>
+
+//             <div className="password-div">
+//                 <label htmlFor="password"></label>
+//                 <input type="text"
+//                     name="password"
+//                     id="password"
+//                     placeholder="Password"
+//                     onChange={handleClick}
+//                     value={formData.password}
+//                 />
+//             </div>
+
+//             <div className="confirmPassword-div">
+//                 <label htmlFor="emailId"></label>
+//                 <input type="text"
+//                     name="confirmPassword"
+//                     id="confirmPassword"
+//                     placeholder="Confirm Password"
+//                     onChange={handleClick}
+//                     value={formData.confirmPassword}
+//                 />
+//             </div>
+
+//             <div className="newsletter-div">
+//                 <input type="checkbox"
+//                     name="isNewsletter"
+//                     id="isNewsletter"
+//                     onChange={handleClick}
+//                     checked={formData.isNewsletter}
+//                 />
+//                 <label htmlFor="isNewsletter">I want to join the newsletter</label>
+//             </div>
+
+//             <div className="signUp">
+//                 <button>Sign Up</button>
+//             </div> </>
+//     )
+// }
